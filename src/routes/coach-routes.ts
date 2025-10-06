787a7b48ce0b681869b3ec86e3c63e4163ed58b9
brunomffl <brunocardoso07@Outlook.com>
2025-10-05 22:10:31 -0300

    feat: Adiciona o middleware de error-handling e ensureAuthenticated para funcionarem corretamente

--- src/routes/coach-routes.ts
+++ src/routes/coach-routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { CoachController } from "@/controller/coach-controller";
 import { validateBody } from "@/middlewares/validate-schema";
 import { createCoachSchema } from "@/schemas/coachSchema";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const coachController = new CoachController()
 const coachRoutes = Router();
 
 coachRoutes.post("/",
+    ensureAuthenticated,
     validateBody(createCoachSchema),
     coachController.create.bind(coachController)
 );
